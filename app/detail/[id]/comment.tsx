ab9700412b94a07df34184b1834e7836f33708c2
meureuet <gmdgjfr@gmail.com>
2023-05-04 23:53:41 +0900

    댓글 보여주기

--- app/detail/[id]/comment.tsx
+++ app/detail/[id]/comment.tsx
@@ -1,16 +1,39 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Comment(props: any) {
-    let [comment, setComment] = useState()
+    let [comment, setComment] = useState('')
+    let [data, setData] = useState([])
     const postId: string = props.postId
+
+    useEffect(() => {
+        fetch(`/api/comment?postId=${postId}`)
+            .then(r => r.json())
+            .then((result) => {
+                setData(result)
+                console.log(data)
+            })
+    }, [])
 
     return (
         <div>
             <div>댓글</div>
             <input onChange={(e) => { setComment(e.target.value as any) }} />
             <button onClick={() => { postComment(postId, comment as any) }}>댓글추가</button>
+
+            {
+                data.length > 0 ?
+                data.map((comment: any, i: number) => {
+                    
+                    return (
+                        <div key={i}>
+                            <p>{comment.author}: {comment.content}</p>
+                        </div>
+                    )
+                })
+                : '댓글 x'
+            }
         </div>
     )
 
--- pages/api/comment.ts
+++ pages/api/comment.ts
@@ -9,10 +9,15 @@ export default async function handler(req: any, res: any) {
     let session = await getServerSession(req, res, authOptions)
 
     if (req.method == 'GET') {
-        /**
-         * 
-         */
+        if(!req.query.postId){
+            return res.status(500).json('게시물 정보 못받아옴')
+        }
 
+        const db: any = (await connectDB).db("forum");
+        let comment = await db.collection("comment").find({"parent": new ObjectId(req.query.postId)}).toArray();
+
+        return res.status(200).json(comment)
+    
     } else if (req.method == 'POST') {
         if (!session) {
             return res.status(500).json('로그인안함')
